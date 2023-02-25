d4e783190f94f3f990e236e78b912373594b3f36
svetlana <milkshake9273@gmai.com>
2023-02-25 15:07:51 +1000

    hometask4 эталон

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// задача 29, программа, которая задает массив из 8 элементов и выводит их на экран
-Task29();
+//Task29();
 
 #region
 static void Task29()
@@ -23,7 +23,7 @@ Console.WriteLine ($"Массив из 8 случайных элементов: 
 
 #region
 
-Task27();
+//Task27();
 int Task27()
 {
 int number = Prompt("Введите число: ");
@@ -33,7 +33,7 @@ return result;
 
 static int Prompt(string message)
 {
-System.Console.WriteLine("Введите число: ");
+System.Console.WriteLine(message);
 int result = Convert.ToInt32(Console.ReadLine());
 return result;
 }
@@ -59,23 +59,74 @@ return res;
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
 #region
-Task25();
-int Task25()
+//Task25();
+void Task25()
 {
 int a = Prompt("Введите число: ");
 int b = Prompt("Введите число: ");
-int result = Degree(a, b);
-return result;
+if (Validate (b))
+{
+System.Console.WriteLine($"{a} в степени {b} = {Degree(a, b)}");
 }
-int Degree( int a, int b)
+// int result = Degree(a, b);
+// return result;
+}
+int Degree(int a, int b)
 {
 int res = a;
 for (int i = 1; i < b; i++)
 {
   res = res * a;
 }
-System.Console.WriteLine($"{a} в степени {b} = {res}");
 return res;
+}
+
+bool Validate (int b)
+{
+  if (b < 0)
+  {
+    System.Console.WriteLine("показатель степени не должен быть меньше 0");
+    return false;
+  }
+  return true;
 }
 #endregion
 
+
+
+// задача 29, программа, которая задает массив из 8 элементов и выводит их на экран
+//Task29();
+
+
+#region
+Task29v2 ();
+void Task29v2()
+{
+  int length = Prompt("Длина массива: ");
+  int min = Prompt("Начальное значение для диапазона: ");
+  int max = Prompt("Конечное значение значение для диапазона: ");
+  int [] array = GenerateArray (length, min, max);
+  PrintArray (array);
+}
+
+
+int [] GenerateArray (int Length, int minValue, int maxValue)
+{
+int [] array = new int [Length];
+Random rran = new Random();
+for (int i = 0; i < Length; i++)
+{
+  array [i] = rran.Next (minValue, maxValue + 1);
+}
+return array;
+
+// string sa = string.Join(", ", array); 
+// Console.WriteLine ($"Массив из 8 случайных элементов: {sa}");
+}
+
+void PrintArray (int[] array)
+{
+  string sa = string.Join(", ", array);
+  System.Console.WriteLine($"[{sa}]");
+}
+#endregion
